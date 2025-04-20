cffb04d65179abae0b55e5e3393104b48f9d43a1
AntonBaglaev <abaglaev@yandex.ru>
2025-04-20 17:24:17 +0300

    order fix end

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -43,17 +43,17 @@ class OrderController extends Controller
     public function handle(OrderFormRequest $request, NewOrderAction $action): RedirectResponse
     {
 
-//        $order = $action($request);
-//
-//        (new OrderProcess($order))->processes([
-//            new CheckProductQuantities(),
-//            new AssignCustomer(request('customer')),
-//            new AssignProducts(),
-//            new ChangeStateToPending(),
-//            new DecreaseProductsQuantities(),
-//            new ClearCart()
-//        ])->run();
-//
+        $order = $action($request);
+
+        (new OrderProcess($order))->processes([
+            new CheckProductQuantities(),
+            new AssignCustomer(request('customer')),
+            new AssignProducts(),
+            new ChangeStateToPending(),
+            new DecreaseProductsQuantities(),
+            new ClearCart()
+        ])->run();
+
         return redirect()
             ->route('home');
     }
